ec57419fd004508a6a3f49eef3cfd8856cb5714b
ObieWalker <obinnawalker@gmail.com>
2018-10-04 09:06:20 +0100

    project structure framework

---
+++ customer-manager/src/App.vue
@@ -0,0 +1,22 @@
+<template>
+  <div id="app">
+    Test
+  </div>
+</template>
+
+<script>
+import HelloWorld from './components/Customers'
+
+export default {
+  name: 'App',
+  components: {
+    
+  }
+}
+</script>
+
+<style>
+#app {
+  
+}
+</style>
